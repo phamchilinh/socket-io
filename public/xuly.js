db6be3593926a7814e65ba734b5c7de757432a00
phamchilinh <phamchilinh20071998@gmail.com>
2022-01-25 19:16:32 +0700

    upload bai 3

--- public/xuly.js
+++ public/xuly.js
@@ -1,17 +1,52 @@
 var socket = io('http://localhost:3000/');
-socket.on('Server-send-login', (data) => {
-    if (data) {
-        $('#loginForm').hide();
-        $('#chatForm').show();
-    }
+socket.on('Server-send-login-fail', () => {
+    alert('User name is existed');
+});
+socket.on('Server-send-login-success', (data) => {
+    $('#loginForm').hide(2000);
+    $('#chatForm').show(1000);
+    $('#currentUser').html(data);
+});
+socket.on('Server-send-users', (data) => {
+    $('#boxContent').html("");
+    data.forEach(element => {
+        $('#boxContent').append(` <div class='userOnline'>${element}</div>`);
+    });
+});
+socket.on('Server-send-message', (data) => {
+    $('#listMessage').append(`<div class='ms'>${data.us} : ${data.ms}</div>`);
+});
+socket.on('Server-send-typing', (data) => {
+    $('#thongbao').html(data +": <img width='30px' src='chat-typing.gif'>");
+});
+socket.on('Server-send-stop-typing', () => {
+    $('#thongbao').html("");
 });
 $(document).ready(() => {
     $('#loginForm').show();
     $('#chatForm').hide();
 
     $('#btnRegister').click(() => {
-        socket.emit('Client-send-A',
+        socket.emit('Client-send-login',
             $('#txtUserName').val()
         );
+    });
+
+    $('#txtMessage').focusin(() => {
+        socket.emit('Client-typing');
+    });
+
+    $('#txtMessage').focusout(() => {
+        socket.emit('Client-stop-typing');
+    });
+
+    $('#btnLogout').click(() => {
+        socket.emit('logout');
+        $('#loginForm').show(2000);
+        $('#chatForm').hide(1000);
+    });
+    $('#btnSend').click(() => {
+        socket.emit('User-send-message', $('#txtMessage').val());
+        $('#txtMessage').val('');
     });
 });
--- server.js
+++ server.js
@@ -11,14 +11,37 @@ const server = http.createServer(app);
 const io = require('socket.io')(server);
 server.listen(3000);
 
+var users = [];
+
 io.on('connection', (socket) => {
     console.log('Hello person :', socket.id);
+    //hiển thị rooms, mặc định mỗi kết nối sẽ tự tạo 1 room mới chứa 1 socket của chính nó.
+    console.log(socket.adapter.rooms);
 
-    socket.on('Client-send-A', (data) => {
-        console.log(data);
-        if (data) {
-            socket.emit('Server-send-login', true);
+    socket.on('Client-send-login', (data) => {
+        if (users.indexOf(data) >= 0) {
+            //fail
+            socket.emit('Server-send-login-fail');
+        } else {
+            //true
+            users.push(data);
+            socket.userName = data;
+            socket.emit('Server-send-login-success', data);
+            io.sockets.emit('Server-send-users', users);
         }
+    });
+    socket.on('logout', () => {
+        users.splice(users.indexOf(socket.userName),1);
+        socket.broadcast.emit('Server-send-users', users);
+    });
+    socket.on('User-send-message', (data) => {
+        io.sockets.emit('Server-send-message', {us: socket.userName, ms: data});
+    });
+    socket.on('Client-typing', () => {
+        socket.broadcast.emit('Server-send-typing', socket.userName);
+    });
+    socket.on('Client-stop-typing', () => {
+        socket.broadcast.emit('Server-send-stop-typing');
     });
 
 });
